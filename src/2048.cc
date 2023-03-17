802b99763e3e2f08c0adcf327adbacd14abff69f
Rickie Blaese <rkyleblaese@gmail.com>
2023-03-16 23:11:21 -0700

    more update

--- src/2048.cc
+++ src/2048.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <random>
@@ -35,64 +34,110 @@ public:
 
     Game(std::int32_t x, std::int32_t y) : x(x), y(y), cells(new std::int64_t*[x]) {
         for (std::int32_t i = 0; i < x; i++) {
-            cells[x] = new std::int64_t[y];
+            cells[i] = new std::int64_t[y];
         }
     }
 
     ~Game() {
         for (std::int32_t i = 0; i < x; i++) {
-            delete[] cells[x];
+            delete[] cells[i];
         }
         delete[] cells;
     }
 
     void move(const Direction& direction) {
         if (direction == Direction::up) {
-            for (std::int32_t i = x - 1; i > 0; i--) {
+            for (std::int32_t i = 1; i < x; i++) {
                 for (std::int32_t j = 0; j < y; j++) {
                     if (cells[i][j] <= 0) { continue; }
-                    if (cells[i - 1][j] == cells[i][j]) {
-                        cells[i - 1][j]++;
-                    } else if (cells[i - 1][j] == 0) {
-                        cells[i - 1][j] = cells[i][j];
+                    std::int32_t z = i - 1;
+                    while (z >= 0) {
+                        if (cells[z][j] > 0) { break; }
+                        z--;
                     }
-                    cells[i][j] = 0;
+                    if (z < 0) { z = 0; }
+                    if (cells[z][j] == cells[i][j]) {
+                        cells[z][j]++;
+                        cells[i][j] = 0;
+                    } else if (cells[z][j] <= 0) {
+                        cells[z][j] = cells[i][j];
+                        cells[i][j] = 0;
+                    } else if (cells[z + 1][j] <= 0) {
+                        cells[z + 1][j] = cells[i][j];
+                        cells[i][j] = 0;
+                    }
                 }
             }
+
         } else if (direction == Direction::down) {
-            for (std::int32_t i = 0; i < x - 1; i++) {
+            for (std::int32_t i = x - 2; i > 0; i--) {
                 for (std::int32_t j = 0; j < y; j++) {
                     if (cells[i][j] <= 0) { continue; }
-                    if (cells[i + 1][j] == cells[i][j]) {
-                        cells[i + 1][j]++;
-                    } else if (cells[i + 1][j] == 0) {
-                        cells[i + 1][j] = cells[i][j];
+                    std::int32_t z = i + 1;
+                    while (z < x) {
+                        if (cells[z][j] > 0) { break; }
+                        z++;
                     }
-                    cells[i][j] = 0;
+                    if (z >= x) { z = x - 1; }
+                    if (cells[z][j] == cells[i][j]) {
+                        cells[z][j]++;
+                        cells[i][j] = 0;
+                    } else if (cells[z][j] <= 0) {
+                        cells[z][j] = cells[i][j];
+                        cells[i][j] = 0;
+                    } else if (cells[z - 1][j] <= 0) {
+                        cells[z - 1][j] = cells[i][j];
+                        cells[i][j] = 0;
+                    }
+
                 }
             }
+
         } else if (direction == Direction::left) {
-            for (std::int32_t j = y - 1; j > 0; j--) {
+            for (std::int32_t j = 1; j < y; j++) {
                 for (std::int32_t i = 0; i < x; i++) {
                     if (cells[i][j] <= 0) { continue; }
-                    if (cells[i][j - 1] == cells[i][j]) {
-                        cells[i][j - 1]++;
-                    } else if (cells[i][j - 1] == 0) {
-                        cells[i][j - 1] = cells[i][j];
+                    std::int32_t z = j - 1;
+                    while (z >= 0) {
+                        if (cells[i][z] > 0) { break; }
+                        z--;
                     }
-                    cells[i][j] = 0;
+                    if (z < 0) { z = 0; }
+                    if (cells[i][z] == cells[i][j]) {
+                        cells[i][z]++;
+                        cells[i][j] = 0;
+                    } else if (cells[i][z] <= 0) {
+                        cells[i][z] = cells[i][j];
+                        cells[i][j] = 0;
+                    } else if (cells[i][z + 1] <= 0) {
+                        cells[i][z + 1] = cells[i][j];
+                        cells[i][j] = 0;
+                    }
+
                 }
             }
+
         } else if (direction == Direction::right) {
-            for (std::int32_t j = 0; j < x - 1 ; j++) {
+            for (std::int32_t j = y - 2; j > 0; j--) {
                 for (std::int32_t i = 0; i < x; i++) {
                     if (cells[i][j] <= 0) { continue; }
-                    if (cells[i][j + 1] == cells[i][j]) {
-                        cells[i][j + 1]++;
-                    } else if (cells[i][j + 1] == 0) {
-                        cells[i][j + 1] = cells[i][j];
+                    std::int32_t z = j + 1;
+                    while (z < y) {
+                        if (cells[i][z] > 0) { break; }
+                        z++;
                     }
-                    cells[i][j] = 0;
+                    if (z >= y) { z = y - 1; }
+                    if (cells[i][z] == cells[i][j]) {
+                        cells[i][z]++;
+                        cells[i][j] = 0;
+                    } else if (cells[i][z] <= 0) {
+                        cells[i][z] = cells[i][j];
+                        cells[i][j] = 0;
+                    } else if (cells[i][z - 1] <= 0) {
+                        cells[i][z - 1] = cells[i][j];
+                        cells[i][j] = 0;
+                    }
+
                 }
             }
         }
@@ -100,21 +145,21 @@ public:
 
 
     void out(WINDOW *win) {
-        std::int32_t max_size = 0, this_size = 0;
+        std::size_t max_size = 0, this_size = 0;
         for (std::int32_t i = 0; i < x; i++) {
             for (std::int32_t j = 0; j < y; j++) {
-                this_size = std::to_string(static_cast<std::int32_t>(std::pow(2, cells[i][j]))).length();
+                this_size = std::to_string(static_cast<std::int64_t>(std::pow(2, cells[i][j]))).length();
                 if (this_size > max_size) {
                     max_size = this_size;
                 }
             }
         }
 
-        std::string this_str;
         for (std::int32_t i = 0; i < x; i++) {
             for (std::int32_t j = 0; j < y; j++) {
-                this_str = static_cast<std::int32_t>(std::pow(2, cells[i][j]));
-                mvwaddstr(win, i * 2, j * max_size + (max_size - this_str.length()) / 2, this_str.c_str());
+                if (cells[i][j] == 0) { continue; }
+                std::string this_str = std::to_string(static_cast<std::int64_t>(std::pow(2, cells[i][j])));
+                mvwaddstr(win, i, j * (max_size + 2) + (max_size + 2 - this_str.length()) / 2, this_str.c_str());
             }
         }
     }
@@ -123,7 +168,11 @@ public:
 
 
 int main() {
-    static constexpr std::int32_t max_input_size = 50; /* this is a number so this is more than large enough */
+    static constexpr std::int32_t max_input_size = 50; /* this is a number input str so this is more than large enough */
+    static constexpr std::int32_t min_random_spawn_count = 1;
+    static constexpr std::int32_t max_random_spawn_count = 3;
+    static constexpr std::int32_t min_random_spawn = 1;
+    static constexpr std::int32_t max_random_spawn = 4;
 
     WINDOW *win = init_ncurses();
     move(0, 0);
@@ -139,7 +188,25 @@ int main() {
     wgetnstr(win, in, max_input_size);
     y = std::atoi(in);
 
+    /* now we are down with full string input */
+    curs_set(0);
+    noecho();
+
     Game game(x, y);
+
+    std::random_device device{};
+    std::default_random_engine engine(device());
+
+    const std::int32_t rscount = std::uniform_int_distribution<>(min_random_spawn_count, max_random_spawn_count)(engine);
+    std::uniform_int_distribution<> xdist(0, x - 1);
+    std::uniform_int_distribution<> ydist(0, y - 1);
+    std::uniform_int_distribution<> powerdist(min_random_spawn, max_random_spawn);
+
+    for (std::int32_t i = 0; i < rscount + 2; i++) {
+        game.cells[xdist(engine)][ydist(engine)] = powerdist(engine);
+    }
+
+    wclear(win);
     game.out(win);
 
     chtype chin = '\0';
@@ -171,13 +238,39 @@ int main() {
                 continue;
         }
 
+        /*
+        std::int32_t exist_count = 0;
+        for (std::int32_t i = 0; i < x; i++) {
+            for (std::int32_t j = 0; j < y; j++) {
+                if (game.cells[i][j] > 0) { exist_count++; }
+                if (exist_count >= x * y) {
+                    break;
+                }
+            }
+        }
+
+        std::int32_t rx = 0, ry = 0;
+        if (exist_count < x * y) {
+            for (std::int32_t i = 0; i < rscount; i++) {
+                rx = xdist(engine);
+                ry = ydist(engine);
+                while (game.cells[rx][ry] > 0) {
+                    rx = xdist(engine);
+                    ry = ydist(engine);
+                }
+
+                game.cells[rx][ry] = powerdist(engine);
+            }
+        }
+        */
+
         wclear(win);
         game.out(win);
         wrefresh(win);
-        
+
     }
-    
+
     deinit_ncurses(win);
     return 0;
-                
+
 }
